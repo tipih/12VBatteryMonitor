bf016a2641aa790dfcb35e4038af1e01e16d7cb2
Michael Rahr <michael.rahr@gmail.com>
2026-01-14 10:55:16 +0100

    CLang-format for mqtt_mgr.h

--- src/comms/mqtt_mgr.h
+++ src/comms/mqtt_mgr.h
@@ -6,13 +6,14 @@
 
 class MqttMgr {
 public:
-  explicit MqttMgr(Client& net) : _client(net) {}
-  void setServer(const char* host, uint16_t port);
-  bool ensureConnected(const char* clientId, const char* user, const char* pass, uint32_t timeoutMs = 7000);
+  explicit MqttMgr(Client &net) : _client(net) {}
+  void setServer(const char *host, uint16_t port);
+  bool ensureConnected(const char *clientId, const char *user, const char *pass,
+                       uint32_t timeoutMs = 7000);
   bool connected() { return _client.connected(); }
-  bool publish(const char* topic, const char* payload, bool retain);
+  bool publish(const char *topic, const char *payload, bool retain);
   void loop() { _client.loop(); }
-  PubSubClient* raw() { return &_client; }
+  PubSubClient *raw() { return &_client; }
 
 private:
   PubSubClient _client;
